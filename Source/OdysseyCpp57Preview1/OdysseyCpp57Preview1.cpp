8e57eb6e54591124c7d16193c77ead1574c3c348
Eric Scholl <eric.scholl@praxinos.coop>
2026-03-18 14:14:12 +0100

    feat: First Commit and basic replacement of 3D Meshes with 2D Animations

---
+++ Source/OdysseyCpp57Preview1/OdysseyCpp57Preview1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OdysseyCpp57Preview1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OdysseyCpp57Preview1, "OdysseyCpp57Preview1" );
